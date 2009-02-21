d6db41c4089696d504292250f8478da2a9ccf742
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-02-21 16:50:09 +0530

    added a ci script

--- ci.sh
+++ ci.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export PATH="$HOME/.gem/ruby/1.8/bin:/usr/java/jdk1.6.0_12/bin:/usr/local/bin:/bin:/usr/bin:$HOME/bin"
-set
-echo Using path: $PATH
+export PATH="/home/rails/.gem/ruby/1.8/bin:/usr/java/jdk1.6.0_12/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/home/rails/bin"
+
 rake setup
 rake
 
