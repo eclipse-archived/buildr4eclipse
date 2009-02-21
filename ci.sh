c44f2ae1e292f18520ea1f54f51d58fd5fd3c254
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-02-21 17:18:07 +0530

    fixing build for cruise

--- ci.sh
+++ ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export HOME="/home/rails"
+export JAVA_HOME="/usr/java/jdk1.6.0_12"
 
-export PATH="$HOME/.gem/ruby/1.8/bin:/usr/java/jdk1.6.0_12/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:$HOME/bin"
+export PATH="$HOME/.gem/ruby/1.8/bin:$JAVA_HOME/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:$HOME/bin"
 echo Using path: $PATH
 
 rake setup
--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -10,7 +10,7 @@
 ###############################################################################
 
 # Point to the buildr source to run with Buildr's source. 
-require File.dirname(__FILE__) + "/../../buildr/spec/spec_helpers.rb"
+require File.expand_path File.join(File.dirname(__FILE__), "/../../buildr/spec/spec_helpers.rb")
 
 require 'ruby-debug'
 Debugger.start
