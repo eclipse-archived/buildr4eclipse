15a05cf217f361443fedf96711e4bb3cd774ae84
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-02-21 17:26:13 +0530

    fixing build for cruise

--- ci.sh
+++ ci.sh
@@ -6,6 +6,8 @@ export JAVA_HOME="/usr/java/jdk1.6.0_12"
 export PATH="$HOME/.gem/ruby/1.8/bin:$JAVA_HOME/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:$HOME/bin"
 echo Using path: $PATH
 
-rake setup
+set
+
+# rake setup
 rake
 
--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -9,8 +9,8 @@
 #     Buildr4Eclipse - initial API and implementation
 ###############################################################################
 
-# Point to the buildr source to run with Buildr's source. 
-require File.expand_path File.join(File.dirname(__FILE__), "/../../buildr/spec/spec_helpers.rb")
+# Point to the buildr source to run with Buildr's source.
+require File.dirname(__FILE__) + "/../../buildr/spec/spec_helpers.rb"
 
 require 'ruby-debug'
 Debugger.start
