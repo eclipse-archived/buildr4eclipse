134b00ff1e534079e25651622d8a305d5ca4b662
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-02-24 00:20:17 +0530

    renamed tasks.rb to package.rb since that is what it is doing

--- lib/buildr4eclipse.rb
+++ lib/buildr4eclipse.rb
@@ -10,7 +10,7 @@
 ###############################################################################
 
 require 'buildr'
-require File.dirname(__FILE__) + '/buildr4eclipse/tasks'
+require File.dirname(__FILE__) + '/buildr4eclipse/package'
 require File.dirname(__FILE__) + '/buildr4eclipse/lib'
 require File.dirname(__FILE__) + '/buildr4eclipse/compiler'
 require File.dirname(__FILE__) + '/buildr4eclipse/plugin_project'
